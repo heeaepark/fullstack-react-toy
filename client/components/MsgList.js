273a0be7c3782babafc9d9486cf71edac6267b94
heeaepark <kiedev06@gmail.com>
2023-05-09 22:50:16 +0900

    feat(msgList): 메시지 리스트 컴포넌트 추가

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
+import MsgInput from "./MsgInput";
 import MsgItem from "./MsgItem";
 
 const userId = ["roy", "jay"];
 const getRandomUserId = () => userId[Math.round(Math.random())]; // userId는 roy / jay 중 랜덤하게 할당
 
-const msgs = Array(50) // 임의의 50개 아이템이 담긴 배열 생성
+const originalMsgs = Array(50) // 임의의 50개 아이템이 담긴 배열 생성
   .fill(0) // map을 돌리기 위해 .fill()을 이용하여 임의의 숫자를 넣고 값을 채운다.
   // map 반복문을 돌려 50개 배열만큼 데이터 생성
   .map((_, i) => ({
@@ -30,12 +32,28 @@ const msgs = Array(50) // 임의의 50개 아이템이 담긴 배열 생성
   }
 ] */
 
-const MsgList = () => (
-  <ul className="messages">
-    {msgs.map((x) => (
-      <MsgItem key={x.id} {...x} />
-    ))}
-  </ul>
-);
+const MsgList = () => {
+  const [msgs, setMsgs] = useState(originalMsgs);
+  const onCreate = (text) => {
+    const newMsg = {
+      id: msgs.length + 1,
+      userId: getRandomUserId(),
+      timestamp: Date.now(), // 임의의 날짜에서 1분단위로 타임스탬프 생성
+      text: `${msgs.length + 1} ${text}`,
+    };
+    setMsgs((msgs) => [newMsg, ...msgs]);
+  };
+
+  return (
+    <>
+      <MsgInput mutate={onCreate} />
+      <ul className="messages">
+        {msgs.map((x) => (
+          <MsgItem key={x.id} {...x} />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default MsgList;
